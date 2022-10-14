0fffa3554d67e3e4602bdb13a855e87eb64ef9cd
Charles <charlesmotsisi100@gmail.com>
2022-10-14 16:18:32 +0200

    Home Added

--- DataAsset/data.js
+++ DataAsset/data.js
@@ -1,8 +1,13 @@
-export const filterRestaurant = [{image:require('../assets/butter.png'),id:'0'},
+export const Data = [{image:require('../assets/butter.png'),id:'0'},
                                  {image:require('../assets/cooking oil.jpg'),id:'1'},
                                  {image:require('../assets/maize.jpg'),id:'2'},
                                  {image:require('../assets/milk.jpg'),id:'3'},
                                  {image:require('../assets/raja.jpg'),id:'4'},
                                  {image:require('../assets/rice.jpg'),id:'5'},
-                                 {image:require('../assets/spagetti.jpg'),id:'6'},
+                                 {image:require('../assets/spagetti.png'),id:'6'},
+];
+
+export const Shops = [{image:require('../assets/shoprite.png'),id:'7'},
+                      {image:require('../assets/makro.jpg'),id:'8'},
+                      {image:require('../assets/picknpay.png'),id:'9'},
 ];
--- components/homePage.jsx
+++ components/homePage.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import { StyleSheet, Text, View, TextInput } from 'react-native'
+import React,{useState} from 'react'
+import { StyleSheet, Text, View, TextInput, FlatList,Image, Button } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome';
-import Icon2 from 'react-native-vector-icons/AntDesign';
 import SelectList from 'react-native-dropdown-select-list'
+import {Data,Shops} from '../DataAsset/data'
+//import { Button } from 'react-native-paper';
+import { TouchableOpacity } from 'react-native-web';
 
 
 const HomePage = () =>{
- 
+  const [indexCheck,setIndexCheck] = useState("0");
+
     return(
         <View style={styles.container}>
             <View style={styles.nav}>
-                  <Icon2 name="profile"
+                  <Icon name="list-alt"
                     size={25}
                     color="#20DC49"
                     style={{marginBottom:60,marginLeft:10}}
-                    />
+                  />
 
-                    <Text style={{fontSize:25,color:'#20DC49',fontWeight:'bold',marginTop:30,marginLeft:2}}>GetEatCheap</Text>
+                    <Text style={{fontSize:25,color:'#20DC49',fontWeight:'bold',marginTop:50,marginLeft:2}}>GetEatCheap</Text>
 
                     <Icon 
                     name="bars"
@@ -44,6 +47,53 @@ const HomePage = () =>{
                 <View style={styles.rightLine}></View>
             </View>
             
+            
+            <View style={{marginTop:15,marginBottom:15}}>
+              <FlatList
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                    style={{marginTop:50,marginLeft:60,marginRight:60}}
+                    data={Data}
+                    keyExtractor={(item)=>item.id}
+                    extraData={indexCheck}
+                    renderItem={({item,index})=>(
+                            <View style={styles.popularPics}>
+                                <Image
+                                style={{height:70,width:70,borderRadius:20,backgroundColor:'#fff',marginLeft:10}}
+                                source={item.image}
+                                />
+                                
+                            </View>
+              
+                    )}
+                     />
+                </View>
+
+                <View style={styles.searchTab2}>
+                    <View style={styles.leftLine2}></View>
+                    <Text style={{color:'#20DC49'}}>Shops</Text>
+                    <View style={styles.rightLine2}></View>
+                </View>
+
+                <View style={styles.shops}>
+                  <Image source={require('../assets/shoprite.png')} style={{width:80,height:80,marginLeft:30,borderRadius:10}}/>
+                  <Image source={require('../assets/picknpay.png')} style={{width:80,height:80,borderRadius:10}}/>
+                  <Image source={require('../assets/makro.png')} style={{width:80,height:80,marginRight:30,borderRadius:10}}/>
+                </View>
+
+                <View style={styles.bottomLine}></View>
+
+                <View style={styles.copyright}>
+                    <Icon
+                    name="copyright"
+                    size={10}
+                    color='#20DC49'
+                    />
+                      <Text style={styles.copyrightTxt}>Copyright GetEatCheap 2022 Terms & Conditions/ Privacy Policy/ Sitemap</Text>
+                </View>
+
+                
+            
   </View>
   )
 }
@@ -59,14 +109,15 @@ const styles = StyleSheet.create({
       nav:{
         width:'100%',
         height:'10%',
-        marginTop:-650,
+        marginTop:-80,
         alignItems:'center',
         flexDirection:'row',
         justifyContent:'space-between'
       },
       borderLine:{
         borderBottomWidth:2,
-        borderBottomColor:'#fff'
+        borderBottomColor:'#fff',
+        marginTop:25,
       },
      searchBar:{
         backgroundColor:'#fff',
@@ -75,7 +126,8 @@ const styles = StyleSheet.create({
         justifyContent:'center',
         width:'70%',
         height:'6%',
-        marginTop:'10%',
+        marginTop:'17%',
+        marginBottom:'8%',
         marginLeft:'15%',
         borderColor:'#20DC49',
         borderWidth:2,
@@ -96,6 +148,49 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         justifyContent:'space-around',
         marginTop:40,
+      },
+      popularPics:{
+        alignItems:'center',
+        flexDirection:'row',
+        justifyContent:'space-around'
+      },
+      searchTab2:{
+        alignItems:'center',
+        flexDirection:'row',
+        justifyContent:'space-around',
+        marginTop:40,
+      },
+      leftLine2:{
+        borderTopWidth:1,
+        borderTopColor:'#fff',
+        width:185,
+      },
+      rightLine2:{
+        borderTopWidth:1,
+        borderTopColor:'#fff',
+        width:185,
+      },
+      shops:{
+        alignItems:'center',
+        flexDirection:'row',
+        justifyContent:'space-between',
+        marginTop:70,
+        marginBottom:20
+      },
+      bottomLine:{
+        borderBottomWidth:1,
+        borderBottomColor:'#fff',
+        marginTop:50
+      },
+      copyright:{
+        alignItems:'center',
+        flexDirection:'row',
+        justifyContent:'center',
+        marginTop:20,
+      },
+      copyrightTxt:{
+        fontSize:10,
+        color:'#20DC49'
       }
      
      
